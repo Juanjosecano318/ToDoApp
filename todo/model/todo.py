81ff12f7a6c8bcc0c1216be62d3f85474052bd75
Juanjosecanog <juanjosecanogiraldo5@gmail.com>
2024-08-26 13:08:46 -0500

    fifth Point: TodoBook class was created

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -13,8 +13,19 @@ class Todo:
     def add_tag(self,tag: str):
         if tag not in self.tags:
             self.tags.append(tag)
+
     def __str__(self) -> str: #retorna un string
-        return f"{self.code} - {self.title}"
+        return f"{self.code_id} - {self.title}"
+
+class TodoBook:
+
+    def __init__(self,todos):
+        self.todos: dict[int, Todo] = {}  #Inicializamos un diccionario vacio
+
+
+
+
+
 
 
 
